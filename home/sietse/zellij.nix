31c76b55aad81b5d9953b068a06516334786336c
Sietse <sietsevanderhoeven@gmail.com>
2026-02-22 20:02:40 +0100

    Auto select helix

--- home/sietse/zellij.nix
+++ home/sietse/zellij.nix
@@ -1,14 +1,13 @@
 {...}: {
   programs.zellij.enable = true;
 
-  
   xdg.configFile."zellij/layouts/dev.kdl".text = ''
     layout {
       pane split_direction="vertical" {
         pane size="20%" command="bacon"
 
         pane split_direction="horizontal" {
-          pane size="75%" command="hx"
+          pane size="75%" command="hx" focus=true
           pane command="nix" {
             args "develop" "-c" "nu"
           }
